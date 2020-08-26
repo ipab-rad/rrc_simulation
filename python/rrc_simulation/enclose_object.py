bcaac9b31c974bf161357f742c602af055700780
Advaith Sai <advaith.maddipatla@gmail.com>
2020-08-26 12:08:09 +0400

    initial pose defined

--- python/rrc_simulation/enclose_object.py
+++ python/rrc_simulation/enclose_object.py
@@ -2,17 +2,34 @@
 """
 This policy should place the end effector of the 3 fingers on the object.
 """
-import numpy as np
-import rrc_simulation
+
 from scipy.spatial.transform import Rotation
 import time
+import numpy as np
 
-_CUBE_WIDTH = 0.065
+import rrc_simulation
 
 platform = rrc_simulation.TriFingerPlatform(visualization=True)
 
-# Current pose of the object
-object_pose = rrc_simulation.trifinger_platform.ObjectPose()
+# Cube pose at time stamp zero
+cube_pose = platform.get_object_pose(0.0)
 
-position = object_pose.position
-orientation = object_pose.orientation
+# Initial position of the cube
+cube_position = cube_pose.position
+
+r = Rotation.from_quat(cube_pose.orientation)
+
+# Initial orientation of the cube as euler angles
+cube_orientation = r.as_euler('xyz')
+
+sim_finger = rrc_simulation.SimFinger('trifingerpro')
+
+# Initial observation
+observation = sim_finger.get_observation(0.0)
+
+# Robot finger pose
+finger_zero_postion = observation.position[:3]
+finger_one_position = observation.position[3:6]
+finger_two_position = observation.position[6:]
+
+_CUBE_WIDTH = 0.065
